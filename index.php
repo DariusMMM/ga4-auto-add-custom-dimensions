0d76073e4f56f6319855c0e7cf84031d30c0f74d
Darius Burgan <darius@dariusburgan.com>
2024-11-15 17:25:13 -0500

    Add quickstart code

--- index.php
+++ index.php
@@ -2,4 +2,22 @@
 
 require 'vendor/autoload.php';
 
-use Google\Analytics\Admin\V1beta\AnalyticsAdminServiceClient;
+putenv('GOOGLE_APPLICATION_CREDENTIALS=C:\Users\Make Model Marketing\Desktop\auto-add-custom-dimensions\reach-analytics2.json');
+
+use Google\Analytics\Admin\V1beta\Account;
+use Google\Analytics\Admin\V1beta\Client\AnalyticsAdminServiceClient;
+use Google\Analytics\Admin\V1beta\ListAccountsRequest;
+
+//$property_id = '466446057';
+
+$client = new AnalyticsAdminServiceClient();
+
+$request = new ListAccountsRequest();
+$response = $client->listAccounts($request);
+
+
+
+//print 'Result:' . PHP_EOL;
+//foreach($response->iterateAllElements() as $account) {
+//    print 'Display name: ' . $account->getDisplayName() . '<br>' . PHP_EOL;
+//}
